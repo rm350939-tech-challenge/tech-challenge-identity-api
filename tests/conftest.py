3e96188cdad7e7a5caff99bddccedc7030ef940f
Jhonatan Pereira <jhonatan.pereira@dock.tech>
2024-12-06 21:45:20 -0300

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,6 @@
 import pytest
 from flask import Flask
 from unittest.mock import MagicMock
-from adapters.http import customer_api
-
-
-@pytest.fixture
-def app():
-    app = Flask(__name__)
-    app.register_blueprint(customer_api, url_prefix="/api/v1")
-    app.config["TESTING"] = True
-    return app
-
 
 @pytest.fixture
 def mock_service(monkeypatch):
