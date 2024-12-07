7e50eb9338558c3443627fa0aba524ce408c21d9
Jhonatan Pereira <jhonatan.pereira@dock.tech>
2024-12-06 22:03:42 -0300

    teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,11 @@ def client():
         yield client
 
 
-def test_root(client):
+def test_root(client, monkeypatch):
+    monkeypatch.setenv("DATABASE_MONGO_USER", "test_user")
+    monkeypatch.setenv("DATABASE_MONGO_PASSWORD", "test_password")
+    monkeypatch.setenv("DATABASE_MONGO_HOST", "test_host")
+    monkeypatch.setenv("DATABASE_MONGO_DATABASE", "test_database")
     response = client.get("/")
     assert response.status_code == 200
     assert response.json == {"project": "Tech Challence - Fase 4"}
